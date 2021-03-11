e71029a301a5964d7467613e8f0b990fe372befe
tisenor007 <77278112+tisenor007@users.noreply.github.com>
2021-03-11 10:11:58 -0400

    Enemy AI behavior change.
    Now has a patrolling function but when the player gets close enemy will chase player.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,19 +7,61 @@ public class Enemy : MonoBehaviour
 {
     public GameObject player;
     private Vector3 playerPosition;
+    private Vector3 enemyPosition;
+    
+
+    [SerializeField]
+    private Transform[] points;
     public NavMeshAgent enemy;
+    private int patrolDestinationPoint = 0;
+    [SerializeField]
+    private float remainingDistance = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy = GetComponent<NavMeshAgent>();
+        enemy.autoBraking = false;
+        GoToNextPoint();
+    }
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            enabled = false;
+            return;
+        }
+
+        enemy.destination = points[patrolDestinationPoint].position;
+        //cycles through points
+        patrolDestinationPoint = (patrolDestinationPoint + 1) % points.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
+        //Debug.Log(distance);
         //constantly sets a vector 3 to players current position
         playerPosition = player.transform.position;
-        //enemy constantly tries to get to that position (vector 3 being set)
-        enemy.SetDestination(playerPosition);
+        enemyPosition = enemy.transform.position;
+        //Debug.Log("player: " + playerPosition);
+        //Debug.Log("Enemy: " + enemyPosition);
+
+
+        if (distance <= 15)
+        {
+            //enemy constantly tries to get to that position (vector 3 being set)
+            Debug.Log("Player and enemy are close");
+            enemy.SetDestination(playerPosition);
+        }
+        else
+        {
+            if (!enemy.pathPending && enemy.remainingDistance < remainingDistance)
+            {
+                GoToNextPoint();
+            }
+        }
+  
     }
 }
