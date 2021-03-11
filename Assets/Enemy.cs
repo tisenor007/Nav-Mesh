6c78bf657aae80cd3de56947f724ae7041233d9a
tisenor007 <77278112+tisenor007@users.noreply.github.com>
2021-03-11 00:30:12 -0400

    basics complete
    map made with an enemy and a player, enemy follows player and avoids obstacles......

---
+++ Assets/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 playerPosition;
+    public NavMeshAgent enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //constantly sets a vector 3 to players current position
+        playerPosition = player.transform.position;
+        //enemy constantly tries to get to that position (vector 3 being set)
+        enemy.SetDestination(playerPosition);
+    }
+}
